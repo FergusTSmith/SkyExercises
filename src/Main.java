1d1c85a957332b23ff35f3d9fcbbd122a60c3fa9
FergusTSmith <FergusTSmith@Gmail.com>
2023-07-21 14:40:56 +0100

    Third Task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
        /*String firstName = "Fergus";
@@ -8,7 +9,7 @@ public class Main {
        String name = firstName + secondName;
        System.out.println(name);
        */
-
+        /*
         int num1 = 100;
         int num2 = 300;
         System.out.println(num1 + num2);
@@ -17,6 +18,21 @@ public class Main {
         System.out.println(num3++);
         System.out.println(num3);
         System.out.println(++num3);
+        */
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please enter your first name");
+        String firstName = s.nextLine();
+        System.out.println("Please enter your second name");
+        String secondName = s.nextLine();
+
+        System.out.println("Hello to you, " + firstName + secondName);
+
+        System.out.println("Please give me two numbers. Number 1:");
+        int firstNumber = s.nextInt();
+        System.out.println("Number 2:");
+        int secondNumber = s.nextInt();
+
+        System.out.println("The sum of your two numbers is: " + (firstNumber + secondNumber));
     }
 }
 
