b261bbda7907e4d0d8221eb9297e03936565b842
FergusTSmith <FergusTSmith@Gmail.com>
2023-07-21 14:33:33 +0100

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,21 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-       String firstName = "Fergus";
+       /*String firstName = "Fergus";
        String secondName = "Smith";
 
        String name = firstName + secondName;
        System.out.println(name);
+       */
 
+        int num1 = 100;
+        int num2 = 300;
+        System.out.println(num1 + num2);
+        int result = num1 / num2;
+        int num3 = 8;
+        System.out.println(num3++);
+        System.out.println(num3);
+        System.out.println(++num3);
     }
 }
 
