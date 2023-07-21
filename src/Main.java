380cfad38a153d083ea756a5e9f05ac7d7394f71
FergusTSmith <FergusTSmith@Gmail.com>
2023-07-21 14:25:14 +0100

    Task 1

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+       String firstName = "Fergus";
+       String secondName = "Smith";
+
+       String name = firstName + secondName;
+       System.out.println(name);
+    }
+}
+
+/*
+ */
